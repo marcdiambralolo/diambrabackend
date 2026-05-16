f580c38c5bf7b3a72a518eb84f3380cae578f8b4
ysprod <yayasidibeproduction@gmail.com>
2026-05-16 21:51:25 +0100

    cinquante trois

--- src/doors-job/doors-job.module.ts
+++ src/doors-job/doors-job.module.ts
@@ -3,6 +3,7 @@ import { BullModule } from '@nestjs/bullmq';
 import { DoorsJobProcessor } from './doors-job.processor';
 import { DoorsJobService } from './doors-job.service';
 import { UsersModule } from '../users/users.module';
+import { ConsultationsModule } from '../consultations/consultations.module';
 import { RubriqueModule } from '../rubriques/rubrique.module';
 
 @Module({
@@ -11,6 +12,7 @@ import { RubriqueModule } from '../rubriques/rubrique.module';
       name: 'user-doors',
     }),
     UsersModule,
+    ConsultationsModule,
     RubriqueModule,
   ],
   providers: [DoorsJobProcessor, DoorsJobService],
